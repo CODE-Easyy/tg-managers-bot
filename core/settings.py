d214bd3548f0d04fc8b8ac8570b9429b8b87f30e
Almazishe <mr.esenyazov@gmail.com>
2020-11-08 07:49:27 +0600

    wasdfg

--- core/settings.py
+++ core/settings.py
@@ -1,16 +1,13 @@
 from pathlib import Path
 import django_heroku
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'dzm_%js3bny^reu9%l4qgicz#rrt0a@fwf__i^91#gqr5uq7va'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -38,7 +35,7 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        
     ),
     'DEFAULT_PERMISSION_CLASSES': {
         'rest_framework.permissions.IsAuthenticated',
